fd7277d1c8760c78ddb18c4410871162b5f4b8a3
Mr-TOPUSiR <85113075+Mr-TOPUSiR@users.noreply.github.com>
2021-06-13 19:32:49 +0545

    Update StringOperation.java

--- Assignments/assignment 5/StringPackage/StringOperation.java
+++ Assignments/assignment 5/StringPackage/StringOperation.java
@@ -1,3 +1,12 @@
+/*
+QUESTION::
+a. Create a class StringOperation with following member function inside package StringPackage.
+  1. boolean isPalindrome(String str): Return true if the input string is palindrome otherwise false
+  2. String reverse(String str): Return the reverse form of input string
+  3. int length(String str): Return length of input string
+  4. String uppercase(String str): Return the uppercase form of input string
+*/
+
 package StringPackage;
 
 public class StringOperation{
